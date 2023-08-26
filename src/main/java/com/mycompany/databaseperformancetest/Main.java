9f20ca18243190452bf9911724945efc3091ba06
JorgeML01 <jorgedd2001@gmail.com>
2023-08-26 14:38:01 -0600

    functions

--- src/main/java/com/mycompany/databaseperformancetest/Main.java
+++ src/main/java/com/mycompany/databaseperformancetest/Main.java
@@ -1,8 +1,49 @@
 package com.mycompany.databaseperformancetest;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(".");
+
+//        Scanner sc = new Scanner(System.in);
+//        DatabaseManager database = null;
+//
+//        System.out.println("Escoja la base de datos con la que trabajará: ");
+//        //String databaseSeleccionada = sc.nextLine();
+//        String databaseSeleccionada = "Firebird";
+//
+//        if (databaseSeleccionada.equals("MariaDB")) {
+//            database = new MariaDBDatabase();
+//        } else if (databaseSeleccionada.equals("PostgreSQL")) {
+//            database = new PostgreSQLDatabase();
+//        } else if (databaseSeleccionada.equals("Firebird")) {
+//            database = new FirebirdDatabase();
+//        } else if (databaseSeleccionada.equals("MySQL")) {
+//            database = new MySQLDatabase();
+//        } else if (databaseSeleccionada.equals("SQL Server")) {
+//            database = new SQLServerDatabase();
+//        }
+//
+//        database.connect();
+//        System.out.println("\n\n\n\n");
+//        ArrayList<String> test = database.getTableNames();
+//
+//        System.out.println("\n\n\n\n");
+//        for (int i = 0; i < test.size(); i++) {
+//            System.out.println("Nombre: " + test.get(i));
+//        }
+//
+//        System.out.println("\n\n\n");
+//        ArrayList<String> fieldNames = database.getTableFields("USUARIOS");
+//        ArrayList<String> filedDataTypes = database.getFieldDataTypes("USUARIOS");
+//
+//        System.out.println("FIELDS:");
+//        for (int i = 0; i < fieldNames.size(); i++) {
+//            System.out.println();
+//            System.out.println("Nombre: " + fieldNames.get(i));
+//            System.out.println("Tipo de dato: " + filedDataTypes.get(i));
+//        }
     }
 }
