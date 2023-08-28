c26493c2bf4d9f9e298636f7469ce2eb11fe312a
JorgeML01 <jorgedd2001@gmail.com>
2023-08-28 16:13:21 -0600

    main update

--- src/main/java/com/mycompany/databaseperformancetest/Main.java
+++ src/main/java/com/mycompany/databaseperformancetest/Main.java
@@ -6,25 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
         
-        String tableName = "PEDIDOS";
-        FirebirdDatabase db = new FirebirdDatabase();
-        db.connect();
-        db.insert(tableName);
-        //ArrayList<String> tables = db.getTableNames(tableName);
-        System.out.println("\n\nTABLE FIELDS");
-        ArrayList<String> tableFields = db.getTableFields(tableName);
-        imprimirArray(tableFields);
-        System.out.println("\n\nDATA TYPES FIELDS");
-        ArrayList<String> dataTypesFields = db.getFieldDataTypes(tableName);
-        imprimirArray(dataTypesFields);
-        System.out.println("\n\n");
-        db.disconnect();
-
-    }
-    
-    public static void imprimirArray(ArrayList<String> list){
-        for (int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i));
-        }
     }
 }
